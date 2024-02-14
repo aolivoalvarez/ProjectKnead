6c760893e0623fbdfc370962f744f5f85cc62298
speakertone <thecolorblindanimator@gmail.com>
2024-02-13 18:59:16 -0600

    Update NewPawTown scene
    - Implement SceneManager into NewPawTown scene
    - Remove InventoryMenuScript.cs from Canvas object
    - Create InventoryManager prefab
    - Edit InventoryMenuScript.cs
      - Remove scene management functions
      - Remove OnEnable and OnDisable functions, since input is already disabled on anything that isn't active
      - Remove pressContinue function since it was redundant

--- Assets/Scripts/InventoryMenuScript.cs
+++ Assets/Scripts/InventoryMenuScript.cs
@@ -1,59 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class InventoryMenuScript : MonoBehaviour
 {
     [SerializeField] GameObject inventoryMenu; //gets inventory menu panel
 
     InventoryInput iInput; //reference to input -- letter E for keyboard
 
-    private void Awake()
+    void Awake()
     {
         iInput = new InventoryInput(); //reference to input
-    }
-
-    private void OnEnable()
-    {
-        iInput.Enable(); //enables input
-    }
-
-    private void OnDisable()
-    {
-        iInput.Disable(); //disables input
     }
     void Start()
     {
         inventoryMenu.SetActive(false); //disables inventory menu on start
+        iInput.Enable();
     }
 
-    private void Update()
+    void Update()
     {
         //keyboard input for opening/closing inventory menu
         if (iInput.Inventory.Open.triggered)
         {
             ToggleInventoryMenu();
         }
-      
     }
 
     public void ToggleInventoryMenu() //toggles inventory menu on or off
     {
         inventoryMenu.SetActive(!inventoryMenu.activeSelf); //turns menu on or off based on previous value
-    }
-
-    public void pressStart() //function for start button -- goes back to title screen
-    {
-        SceneManager.LoadSceneAsync("TitleScreen"); //opens TitleSreen scene
-    }
-    
-    public void pressContinue() //function for continue button -- toggles inventory menu to take it off
-    {
-        ToggleInventoryMenu();
-    }
-
-    public void pressExit() //function for exit button -- goes to game over screen
-    {
-        SceneManager.LoadSceneAsync("GameOverScreen"); //opens GameOverScreen scene
     }
 }
