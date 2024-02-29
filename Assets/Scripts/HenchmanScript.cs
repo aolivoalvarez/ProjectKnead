d9741b0aa9dca3ff02e5a85cd7887a68ee2e0238
Alex Olivo <alexis.olivo.alvarez@gmail.com>
2024-02-29 09:38:25 -0600

    some henchman script work
    -- unfinished, i just need to pull

--- Assets/Scripts/HenchmanScript.cs
+++ Assets/Scripts/HenchmanScript.cs
@@ -8,13 +8,45 @@ public class HenchmanScript : MonoBehaviour
     [SerializeField] int maxHealth = 4;
     [SerializeField] int attackDamage = 2;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] string group;
+    [SerializeField] string animal;
     BoxCollider2D boxCollider;
     Rigidbody2D rigidBody;
     Animator animator;
 
     void Start()
     {
+
+        switch (group)
+        {
+            case "HenchGroup1":
+                {
+                    maxHealth = 4;
+                    attackDamage = 2;
+                    break;
+                }
+            case "HenchGroup2":
+                {
+                    maxHealth = 8;
+                    attackDamage = 4;
+                    break;
+                }
+            case "HenchanGroup3":
+                {
+                    maxHealth = 12;
+                    attackDamage = 8;
+                    break;
+                }
+            default:
+                {
+                    maxHealth = 4;
+                    attackDamage = 2;
+                    break;
+                }
+        }
+
         health = maxHealth;
+
         boxCollider = GetComponent<BoxCollider2D>();
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -23,5 +55,20 @@ public class HenchmanScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+            player.DecreaseHealth(attackDamage);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
     }
 }
