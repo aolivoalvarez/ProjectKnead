e00097f2e78b809a84b110b45b4fccb5b717f5f8
LilChode331 <drodriguez092644@gmail.com>
2024-04-03 09:23:33 -0500

    1080x1920Graphic-Door-30x50/NpcScript-Updated

--- Assets/Scripts/NPC's/Npc1.cs
+++ Assets/Scripts/NPC's/Npc1.cs
@@ -11,57 +11,65 @@ using System;
 
 public class Npc1 : MonoBehaviour
 {
-    [SerializeField] Transform[] points;
+    // [Variables]
+    [SerializeField , Tooltip("Destination points")] Transform[] points;
     public int targetPoints;
-    public float speed;
-    bool Out;
+    [SerializeField]public float speed;
+    bool Outwalk;
+    [SerializeField, Tooltip("Pause time after arriving at destination points")]float waitTime =  2.0f;
+    float elapsedTime = 0.0f;
    
   
     
-    void Start()
-    {
+    void Start(){
        
         targetPoints = 0;
-        Out = true;
+        Outwalk = true;
     }
 
-    void Update()
-    {
-        if(transform.position == points[targetPoints].position)
-       {
-        NextTarget();
-       }
-       transform.position = Vector3.MoveTowards(transform.position, points[targetPoints].position, speed * Time.deltaTime);
+    void Update(){
+        // Move 
+        transform.position = Vector3.MoveTowards(transform.position, points[targetPoints].position, speed * Time.deltaTime);
+
+        // Upon reaching destination move to next 
+        if(transform.position == points[targetPoints].position){
+        
+            // Integer Countdown
+            elapsedTime += Time.deltaTime;
+
+            if(elapsedTime >= waitTime){
+
+                    elapsedTime = 0;
+                
+
+                if(Outwalk == true){   
+                    targetPoints++;
+                }
+
+                
+                if( targetPoints >= points.Length){
+                    Outwalk = false;
+                }
+
+            
+                if(Outwalk == false){
+                    targetPoints--;
+                }
+
+
+                if(targetPoints <= 0 ){
+                    Outwalk = true;
+                }
+            
+
+            }
+        
+        }
+       
+    }
+
+      
        
        
       
-    }
-    void NextTarget()
-    {
-        if(Out == true)
-        {
-            targetPoints++;
-        }
-        
-        if( targetPoints >= points.Length )
-        {
-            Out = false;
-            
-        }
-        
-        if(Out == false)
-        {
-            targetPoints--;
-        }
-
-        if(targetPoints <= 0 )
-        {
-            Out = true;
-        }
-
-        
-       
-        
-    }
-    
 }
