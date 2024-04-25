c430eb54c43ddf25317b7724964dff92f979c5a6
speakertone <thecolorblindanimator@gmail.com>
2024-04-25 15:46:34 -0500

    Implement enemy spawns in Dungeon0

--- Assets/Scripts/Environment/FallingItemScript.cs
+++ Assets/Scripts/Environment/FallingItemScript.cs
@@ -12,31 +12,41 @@ public class FallingItemScript : MonoBehaviour
 {
     [SerializeField] GameObject shadowPrefab;
     [SerializeField] float timeToFall;
-    public GameObject storedItem { get; set; }
-    public float groundLevelY { get; set; }
+    public GameObject storedItem;
+    public float groundLevelY;
     float offsetY;
 
-    void Start()
+    void OnEnable()
     {
-        GetComponentInChildren<Collider2D>().enabled = false; // while an item is falling, it should have no collision
-        offsetY = GetComponentInChildren<SpriteRenderer>().bounds.size.y * 0.5f;
-        GetComponentInChildren<SpriteRenderer>().sortingLayerName = "Entity";
-        GetComponentInChildren<SpriteRenderer>().sortingOrder = 0;
-        Instantiate(shadowPrefab, new Vector2(transform.position.x, groundLevelY), Quaternion.identity, transform); // creates a child object from the shadow prefab
-        StartCoroutine(FallRoutine(storedItem.transform.DOMoveY(groundLevelY + offsetY, timeToFall).SetEase(AnimationCurvesScript.instance.fallingItem)));
+        if (AnimationCurvesScript.instance != null)
+        {
+            GetComponentInChildren<Collider2D>().enabled = false; // while an item is falling, it should have no collision
+            offsetY = GetComponentInChildren<SpriteRenderer>().bounds.size.y * 0.5f;
+            GetComponentInChildren<SpriteRenderer>().sortingLayerName = "Entity";
+            GetComponentInChildren<SpriteRenderer>().sortingOrder = 0;
+            Instantiate(shadowPrefab, new Vector2(transform.position.x, groundLevelY), Quaternion.identity, transform); // creates a child object from the shadow prefab
+            StartCoroutine(FallRoutine(storedItem.transform.DOMoveY(groundLevelY + offsetY, timeToFall).SetEase(AnimationCurvesScript.instance.fallingItem)));
+        }
     }
 
     public IEnumerator FallRoutine(Tween fallTween)
     {
         GetComponentInChildren<Collider2D>().enabled = false;
+        if (storedItem.GetComponent<BasicEnemyScript>() != null)
+        {
+            storedItem.GetComponent<BasicEnemyScript>().agent.enabled = false;
+        }
+
         while (fallTween.IsActive())
         {
             yield return new WaitForEndOfFrame();
         }
         GetComponentInChildren<Collider2D>().enabled = true;
-        storedItem.transform.SetParent(null);
+        storedItem.transform.SetParent(transform.parent);
         if (storedItem.GetComponent<Pickup>() != null)
             storedItem.GetComponent<Pickup>().StartDespawnRoutine();
+        if (storedItem.GetComponent<BasicEnemyScript>() != null)
+            storedItem.GetComponent<BasicEnemyScript>().agent.enabled = true;
         Destroy(gameObject);
     }
 }
